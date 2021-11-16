4bdeff5d1784b65de02309307f8c6a723bd72390
emylincon <emylincon@gmail.com>
2021-11-16 22:13:02 +0000

    add loadbalancer

--- create_pipeline/loadbalancer.tf
+++ create_pipeline/loadbalancer.tf
@@ -18,6 +18,10 @@ resource "aws_lb_target_group" "lb_target" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.web_vpc.id #var.vpc_default_id # # aws_vpc.web_vpc.id
+
+  depends_on = [
+    aws_lb.loadbalancer
+  ]
 }
 
 resource "aws_lb_listener" "front_end" {
