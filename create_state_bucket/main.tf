a420ed7086622ffa50d83834e120aef0e46c489e
emylincon <emylincon@gmail.com>
2021-11-16 08:13:28 +0000

    force delete bucket

--- create_state_bucket/main.tf
+++ create_state_bucket/main.tf
@@ -8,8 +8,9 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "my-tf-state-bucket-emeka"
-  acl    = "private"
+  bucket        = "my-tf-state-bucket-emeka"
+  acl           = "private"
+  force_destroy = true
 
   tags = {
     Name        = "My bucket"
